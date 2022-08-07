1d72bb78835b52426db0142f9413f581b47f7b1a
DanialKh <danial.khosrojerdi@gmail.com>
2022-08-07 20:43:45 +0430

    splash screen build

--- lib/screens/splashscreen.dart
+++ lib/screens/splashscreen.dart
@@ -1,14 +1,40 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-class SplashScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:techblog_danivar/screens/homescreen.dart';
+
+class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[100],
       body: Center(
-        child: Image.asset('assets/images/logo_splash.png'),
+        child: Image.asset('assets/images/logo_splash.png', width: 200),
       ),
     );
+  }
+
+  void getData() {
+    Timer(const Duration(seconds: 3), () {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const HomeScreen(),
+        ),
+      );
+    });
   }
 }
